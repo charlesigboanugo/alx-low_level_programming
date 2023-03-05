207cc994283a4b17a0882e3d52b39f55a0847bf6
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-03-05 09:57:32 -0800

    edited

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,87 +1,68 @@
-#include <stdio.h>
+#include <math.h>
 #include "main.h"
 
 /**
-* _islower - A function that checks for lowercase character
-* @c: c is an integral charcater
-* Return: returns 1 if c is lowercase and 0 otherwise
-*/
+ * power - exponents
+ * @base: base
+ * @exp: exponent
+ * Return: result (int)
+ */
 
-void print_number(int n)
+int  power(int base, int exp)
 {
-	int abs_int, remainder, pow_of_int, whole_num, divisor;
+	int i, num;
 
-	if (n != 0)
-	{
-		if (n < 0)
-		{
-			abs_int = -1 * n;
-			_putchar('-');
-		}
-		else 
-		{	
-			abs_int = n;
-		}
+	num = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
 
-		pow_of_int = 1;
-		whole_num = abs_int;
-		while (whole_num > 0)
-		{
-			if (whole_num / 10 >= 1)
-			{
-				pow_of_int *= 10;
-			}
-			
-			whole_num = whole_num / 10;
-
-		}
-
-		divisor = pow_of_int;
-		remainder = abs_int;
-		while (divisor >= 1)
-		{
-			_putchar('0' + remainder / divisor);
-			remainder = remainder % divisor;
-			divisor /= 10;
-		}
-		
-
-	}
-	
-	else
-	{
-		_putchar('0');
-	}
-
-	_putchar('\n');
-
-
+	return (num);
 }
 
 /**
-* main - calls a function that checks for lowercase character.
-* Return: Always 0.
-*/
+ * print_number - prints an integer
+ * @n: number to print
+ * Return void
+ */
 
-int main(void)
+void print_number(int n)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	print_number(-9890845);
-	_putchar('\n');
-	print_number(26057);
-	_putchar('\n');
-	print_number(9081);
-	_putchar('\n');
-	print_number(-6);
-	_putchar('\n');
-	return (0);
+	int negative = 0;
+	int digit;
+	int divisor;
+	int begin = 0;
+	int place = 10;
+
+	if (n < 0)
+	{
+		negative = 1;
+		n = n * -1;
+	}
+	while (place >= 0)
+	{
+		/*divisor = pow(10, place);*/
+		divisor = power(10, place);
+		digit = ((n / divisor) % 10);
+		if (digit == 0 && begin == 0)
+		{
+			place--;
+		}
+		else if (digit != 0 && begin == 0)
+		{
+			begin = 1;
+			if (negative == 1)
+				_putchar('-');
+			_putchar('0' + digit);
+			place--;
+		}
+		else
+		{
+			_putchar('0' + digit);
+			place--;
+		}
+	}
+	if (digit == 0 && divisor == 1)
+	{
+		_putchar(48);
+	}
 }
