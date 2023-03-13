cefac52f45b4f8cfb9654c43d7ab077ab68f8a66
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-03-13 00:52:31 -0700

    finally

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,20 @@
 
 int main(void)
 {
-	unsigned long temp2, last, sec_last;
-	unsigned long cut1A, cut1B, cut2A, cut2B, left, right;
-	int count, temp;
+	unsigned long last, sec_last, temp;
+	unsigned long cut1A, cut1B, cut2A, cut2B, left, right, temp2, limit;
+	int count  = 3;
 
-	count = 3;
 	printf("1, 2, ");
 	last = 2;
 	sec_last = 1;
+	limit = 1000000000000000000;
 	while (count <= 98)
 	{
-		if (last + sec_last  > 10)
+		if (last + sec_last  > limit)
 		{
-			right = (cut1B + cut2B) % 10;
-			temp = (cut1B + cut2B) / 10;
+			right = (cut1B + cut2B) % limit;
+			temp = (cut1B + cut2B) / limit;
 			left = (cut1A + cut2A) + temp;
 			printf("%lu%lu", left, right);
 			cut1A = cut2A;
@@ -34,10 +34,10 @@ int main(void)
 			last = last + sec_last;
 			sec_last = temp2;
 			printf("%lu", last);
-			cut2A = last / 10;
-			cut2B = last % 10;
-			cut1A = sec_last / 10;
-			cut1B = sec_last % 10;
+			cut2A = last / limit;
+			cut2B = last % limit;
+			cut1A = sec_last / limit;
+			cut1B = sec_last % limit;
 		}
 		if (count < 98)
 			printf(", ");
