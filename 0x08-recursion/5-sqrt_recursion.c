6f68730a6322176f569dd61df2b2b5ba10cda303
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-03-21 02:31:57 -0700

    finnally

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,8 @@
  * check_int - checks integer as candidate for square root
  * @sqr: square of integer
  * @x: integer
- * Return: 1 if
+ * Return: square root unless x is less than 1
+ * or not a perfect square
  */
 
 int check_int(int x, int sqr)
@@ -13,8 +14,16 @@ int check_int(int x, int sqr)
 		return (-1);
 	if (sqr * sqr == x)
 		return (sqr);
-	return check_int(x, ++sqr);
+	return (check_int(x, ++sqr));
 }
+
+/**
+ * _sqrt_recursion - gets the natural square root of a number.
+ * @n: an integer number
+ *
+ * Return: the square root of n if n is a perfect square else return -1
+ */
+
 int _sqrt_recursion(int n)
 {
 	int sqrt_check;
