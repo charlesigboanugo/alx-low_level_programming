1e2e418ce82787983c4e14ac86e4656b41f127e3
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-03-22 08:42:46 -0700

    very great

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - prints the chessboard
@@ -10,12 +11,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for(i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			printf("%c", *(a[i] + j));
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard2.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_chessboard - prints the chessboard
@@ -11,12 +10,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for(i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", *(a[i] + j));
+			_putchar(a[i][j]);
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
