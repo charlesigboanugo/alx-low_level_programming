faa870b72597c4745a78b1e1bcb64808b2b31b6f
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-03-24 02:23:13 -0700

    poor

---
+++ 0x0B-malloc_free/0-create_array2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of size size
+ *
+ * @size: size of array
+ * @c: char to fill array with
+ *
+ * Return: pointer to first element
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(*ptr) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < 98; i++)
+		ptr[i] = c;
+
+	return (ptr);
+}
