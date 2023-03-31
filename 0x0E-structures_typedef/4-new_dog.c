3d491b887a4f81d3959de6cabfb3f333098c380f
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-03-31 12:42:30 -0700

    good

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,42 +1,53 @@
 #include "dog.h"
 #include <stdlib.h>
 
-size_t _strlen(char *s)
-{
-	size_t len = 0;
-
-	for (; *s != 0; s++)
-		len++;
-
-	return (len);
-}
-
-void *_allocate(char *elem, size_t size)
-{
-	char *ptr, *res;
-
-	ptr = malloc(sizeof(*ptr) * (size + 1));
-	res = ptr;
-	if (!ptr)
-		return (NULL);
-	for (; *elem != 0; ptr++, elem++)
-		*ptr = *elem;
-
-	return (res);
-
-}
-
+/**
+ * new_dog - makes a dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newdog;
-	
-	newdog = malloc(sizeof(dog_t));
-	if (!newdog)
-		return(NULL);
+	dog_t *d;
+	int len;
+	char *ptr;
 
-	newdog->age = age;
-	newdog->name = _allocate(name, _strlen(name));
-	newdog->owner = _allocate(owner, _strlen(owner));
+	if (name == 0 || owner == 0)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-	return (newdog);
+	for (len = 1, ptr = name; *ptr; len++)
+		ptr++;
+	d->name = malloc(len);
+	if (d->name == 0)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	for (len = 1, ptr = owner; *ptr; len++)
+		ptr++;
+	d->owner = malloc(len);
+	if (d->owner == 0)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	for (len = 0; *name != 0; len++, name++)
+		d->name[len] = *name;
+	d->name[len] = 0;
+	for (len = 0; *owner != 0; len++)
+		d->owner[len] = *owner++;
+	d->owner[len] = 0;
+	d->age = age;
+
+	return (d);
 }
