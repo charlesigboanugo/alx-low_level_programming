1368bae2906a42b5d97a8537d925801cc1f67033
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-04-13 00:05:05 -0700

    no

--- 0x13-more_singly_linked_lists/check.c
+++ 0x13-more_singly_linked_lists/check.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
+/**
+ * pop_listint - returns value of head node and removes it,
+ * 0 otherwise
+ *
+ * @head: head of list
+ *
+ * Return: value of node, or 0 if list empty
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *ptr;
 	int data = 0;
-	
+
 	if (head != NULL && *head != NULL)
 	{
 		data = (*head)->n;
