c59a9ee47809f46ac24170106aed63dd30fc4e21
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-04-21 10:07:39 -0700

    testing

---
+++ 0x14-bit_manipulation/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+print_binary - prints the binary form of a number
+* @n: an unsigned int
+*
+* Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	if ((n >> 1) > 0)
+		print_binary(n >> 1);
+	printf("%lu", n - ((n >> 1) << 1));
+}
