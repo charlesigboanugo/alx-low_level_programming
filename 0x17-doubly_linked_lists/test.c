be9f7a1496861725b453f90e9733e289f65f44f8
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-06-04 16:45:38 +0000

    test

--- 0x17-doubly_linked_lists/test.c
+++ 0x17-doubly_linked_lists/test.c
@@ -1,20 +1,27 @@
 #include <stdlib.h>
 #include "lists.h"
-#include <stdio.h>
 
 /**
-* print_dlistint - prints all the elements of a doubly linked list
-* @h: head of the list
+* add_dnodeint - adds a new node at the beginning of a doubly linked list
+* @head: head of the list
+* @n: an integer
 *
-* Return: number of nodes in the list
+* Return: address of the new node
 */
-size_t print_dlistint(const dlistint_t *h)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t len;
+	dlistint_t *new;
 
-	for (len = 0; h != NULL; h = h->next)
-	{
-		len++;
-	}
-	return (len);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->next = *head;
+	new->prev = NULL;
+	new->n = n;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }
