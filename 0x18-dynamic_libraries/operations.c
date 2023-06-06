e4f519fb52498211f484313945b9a2f683591956
charlesigboanugo <charlesigboanugo033@gmail.com>
2023-06-06 02:38:36 -0700

    test

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,12 +1,8 @@
-#include "Python.h"
+#include <stdio.h>
 
-long int add(PyObject *a, PyObject *b)
+long int add(long int a, long int b)
 {
-	long int op1, op2;
-	
-	op1 = PyLong_AsLong(a);
-	op2 = PyLong_AsLong(b);
-	printf("%li\n", op1 + op2);
-	return (op1 + op2);
-	
+    long int sum = a + b;
+    printf("Sum: %ld\n", sum);
+    return sum;
 }
