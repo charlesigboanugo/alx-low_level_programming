20595a31aa00978507d38a5d4de433fce817b98e
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-03 07:02:04 +0100

    great

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,65 +1,27 @@
 #!/usr/bin/python3
-"""island_perimeter function"""
-
-
-def try_the_edges(r, c, grid):
-    """checks perimeter of individual cells for water (0)
-    Args:
-        r (int): row coordinate
-        c (int): column coordinate
-        grid (list): list of lists
-    Returns:
-        count of zeroes or sides cell is surrounded by water
-    """
-    count = 0
-    if r == 0 or grid[r - 1][c] == 0:
-        count += 1
-    if r == len(grid) - 1 or grid[r + 1][c] == 0:
-        count += 1
-    if c == 0 or grid[r][c - 1] == 0:
-        count += 1
-    if c == len(grid[0]) - 1 or grid[r][c + 1] == 0:
-        count += 1
-
-    return count
+"""Calculate perimeter of a grid island"""
 
 
 def island_perimeter(grid):
-    """perimeter of island (1) surrounded by water (0)
-    Args:
-        grid (list): list of lists
-    Returns:
-        perimeter number of cells the island is surrounded by water
+    """Calculate perimeter of a grid island. Grid is a list of equal sized
+    lists.
     """
+    height = len(grid) - 1
+    if height == 0:
+        return 0
+    width = len(grid[0]) - 1
+    if width == 0:
+        return 0
     perimeter = 0
-
-    if not grid:
-        return perimeter
-    if type(grid) is not list:
-        return perimeter
-    rows = len(grid)
-    if rows < 1:
-        return perimeter
-    columns = len(grid[0])
-
-    for r in range(rows):
-        for c in range(columns):
-            if grid[r][c] == 1:
-                perimeter += try_the_edges(r, c, grid)
-
+    for x in range(width + 1):
+        for y in range(height + 1):
+            if grid[y][x] == 1:
+                if x == width or grid[y][x + 1] == 0:
+                    perimeter += 1
+                if x == 0 or grid[y][x - 1] == 0:
+                    perimeter += 1
+                if y == height or grid[y + 1][x] == 0:
+                    perimeter += 1
+                if y == 0 or grid[y - 1][x] == 0:
+                    perimeter += 1
     return perimeter
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-
-    grid = [
-        [1, 1],
-        [1, 1]
-    ]
-    print(island_perimeter(grid))
--- 0x1C-makefiles/README.md
+++ 0x1C-makefiles/README.md
@@ -1,7 +1,8 @@
-# C - Makefiles
-* 0-Makefile: make file that creates holberton executable with all rule and prints seahorse
-* 1-Makefile: make file that creates holberton executable with all rulle adn variables
-* 2-Makefile: make file that creates holberton executable only the updated source files
-* 3-Makefile: make file that recompiles only updated source files, clean, oclean, fclean, re never fail
-* 4-Makefile: make file that is complete
-* 5-island_perimeter.py: function that returns the perimeter of the island
+#A set of projects involving makefiles
+1. 0-Makefile: use the all rule
+2. 1-Makefile: as above and use CC and SRC variables
+3. 2-Makefile: as above and use OBJ and NAME variables
+4. 3-Makefile: as above, plus clean, oclean, fclean, re and RM
+5. 4-Makefile: as above, plus CFLAGS
+6. 5-island-perimeter.py: calculate perimeter of island in grid map
+7. 100-Makefile: as 4-Makefile, but using implicit rules
